9136f1ee33cd2a4144c34d71c724e25f1a5944ca
Kari Palenius <kari.palenius@aalto.fi>
2013-03-29 17:09:24 +0200

    Fixed unit tests to fail more quickly (unless there is a timeout of course)

--- UnitTests/NoppaAPITests.cs
+++ UnitTests/NoppaAPITests.cs
@@ -20,9 +20,11 @@ namespace UnitTests
 
         [TestMethod]
         [Asynchronous]
-        public async Task TestGetAllOrganizations()
+        public async void TestGetAllOrganizations()
         {
             List<Organization> organizations = await NoppaAPI.GetAllOrganizations();
+
+            Assert.IsNotNull(organizations, "Cannot fetch organizations, either not found or timed out");
 
             Assert.IsTrue(organizations.Exists( (item) => item.Id == "CHEM" ));
             Assert.IsTrue(organizations.Exists( (item) => item.Id == "ECON" ));
@@ -31,6 +33,21 @@ namespace UnitTests
             Assert.IsTrue(organizations.Exists( (item) => item.Id == "ERI" ));
             Assert.IsTrue(organizations.Exists( (item) => item.Id == "SCI" ));
             Assert.IsTrue(organizations.Exists( (item) => item.Id == "TaiK" ));
+
+            EnqueueTestComplete();
+        }
+
+        [TestMethod]
+        [Asynchronous]
+        public async void TestGetDepartment()
+        {
+            string dept = "T4070";
+            Department department = await NoppaAPI.GetDepartment(dept);
+
+            Assert.IsNotNull(department, "Cannot fetch department, either not found or timed out");
+            Assert.AreEqual("ELEC", department.OrgId);
+            Assert.AreEqual(dept, department.Id);
+            Assert.AreEqual("Department of Communications and Networking", department.name_en);
 
             EnqueueTestComplete();
         }
