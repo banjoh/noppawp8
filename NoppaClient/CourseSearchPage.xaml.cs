1067d2ecd89ffb3057cb4b856f408a07cec3e49c
Christoffer Ventus <christoffer.ventus@aalto.fi>
2013-04-12 21:03:51 +0300

    Give search box focus when navigating to search page.

--- NoppaClient/CourseSearchPage.xaml.cs
+++ NoppaClient/CourseSearchPage.xaml.cs
@@ -19,26 +19,10 @@ namespace NoppaClient
         public CourseSearchPage()
         {
             InitializeComponent();
-        }
-
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            base.OnNavigatedTo(e);
             _viewModel = new CourseListViewModel(new PhoneNavigationController());
             DataContext = _viewModel;
-        }
 
-        private void LongListSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            var list = sender as LongListSelector;
-            if (list != null)
-            {
-                var selection = list.SelectedItem as Course;
-                if (selection != null)
-                {
-                    NavigationService.Navigate(new Uri("/CoursePage.xaml?id=" + HttpUtility.UrlEncode(selection.Id), UriKind.Relative));
-                }
-            }
+            this.Loaded += (o, e) => SearchBox.Focus();
         }
     }
 }
