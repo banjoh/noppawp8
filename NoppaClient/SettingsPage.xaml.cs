1a4018ff7729d32f02b58f1b7bc112940891037a
Christoffer Ventus <christoffer.ventus@aalto.fi>
2013-04-12 20:55:30 +0300

    Bind PrimaryTileIsActive to boolean, and not through events.

--- NoppaClient/SettingsPage.xaml.cs
+++ NoppaClient/SettingsPage.xaml.cs
@@ -13,35 +13,6 @@ namespace NoppaClient
         public SettingsPage()
         {
             InitializeComponent();
-
-            SetToggleValue(App.Settings.PrimaryTileIsActive);
-        }
-
-        private void updateTile_Toggle(object sender, RoutedEventArgs e)
-        {
-            var toggleSwitch = sender as ToggleSwitch;
-            if (toggleSwitch != null)
-            {
-                App.Settings.PrimaryTileIsActive = (bool)toggleSwitch.IsChecked.Value;
-                
-                // Update value once the background agent has been started.
-                // It may fail so the ToggleSwitch may need to be updated
-                SetToggleValue(App.Settings.PrimaryTileIsActive);
-            }
-        }
-
-        private void SetToggleValue(bool value)
-        {
-            EventHandler<RoutedEventArgs> h = new EventHandler<RoutedEventArgs>(updateTile_Toggle);
-            // Temporarily unhook the event handler
-            updateTile.Checked -= h;
-            updateTile.Unchecked -= h;
-
-            updateTile.IsChecked = value;
-            
-            // Reconnect the event handler
-            updateTile.Checked += h; // DOES NOT SEEM TO SUPPRESS THIS EVENT
-            updateTile.Unchecked += h;  
         }
     }
 }
